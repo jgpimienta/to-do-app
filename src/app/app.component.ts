56945953fe540aa9b1f0cfab38b870c84cb364cc
edwin439 <edwinechavarria439@gmail.com>
2023-02-13 13:32:27 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  title = 'todo-app';
+
+  tasks: string[] = [];
+  newTask: string = ''; 
+ 
+
+  eliminarTask(taskIndex: number) {
+    this.tasks.splice(taskIndex, 1);
+  }
+  addTask(){
+   
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+      alert("se ha agredo una nueva tarea")
+    
+  }
+ 
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TaskComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
